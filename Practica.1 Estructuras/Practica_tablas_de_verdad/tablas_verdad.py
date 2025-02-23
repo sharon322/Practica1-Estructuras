d70bd6a9a65be6c3434a56eaab6796ec3d928d54
sharon322 <nojsharon@umes.edu.gt>
2025-02-23 14:30:56 -0600

    Add files via upload

---
+++ Practica.1 Estructuras/Practica_tablas_de_verdad/tablas_verdad.py
@@ -0,0 +1,8 @@
+class TablaVerdad:
+    operadores = {
+        '^': lambda p, q: p and q,
+        '|': lambda p, q: p or q,
+        '⊕': lambda p, q: p != q,
+        '→': lambda p, q: (not p) or q,
+        '←→': lambda p, q: p == q
+    }
