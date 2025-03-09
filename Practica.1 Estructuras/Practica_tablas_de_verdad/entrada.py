94fb99511016a2b8e6ec7e0c4ec8622569842e66
sharon322 <nojsharon@umes.edu.gt>
2025-03-08 20:42:57 -0600

    y mas cambios

--- Practica.1 Estructuras/Practica_tablas_de_verdad/entrada.py
+++ Practica.1 Estructuras/Practica_tablas_de_verdad/entrada.py
@@ -1,14 +1,21 @@
 def obtener_entrada(mensaje, max_elementos):
+    # Solicita la entrada del usuario y asegura que esté en el formato correcto
     while True:
+        # Solicita la entrada del usuario, elimina espacios en blanco y divide por comas
         entrada = input(mensaje).replace(" ", "").split(',')
+
+        # Verifica que la cantidad de elementos esté dentro del rango permitido
         if 1 <= len(entrada) <= max_elementos:
             # Verifica que todos los elementos sean letras
             if all(e.isalpha() for e in entrada):
                 return list(entrada)  # Devuelve la lista de elementos, manteniendo el orden
             else:
-                print("Error: Todos los elementos deben ser letras.")
+                print("Error: Solo se pueden ingresar letras.")
         else:
+            # Mensaje de error si la cantidad de elementos no es válida
             print(f"Error: Debe ingresar entre 1 y {max_elementos} elementos separados por comas.")
 
-#replace= (' ')sustituye todos los espacios en blanco en la entrada del usuario por una cadena vacía, efectivamente eliminándolos.
-#split(',') divide la cadena resultante en una lista de elementos utilizando la coma como delimitador.
+
+# replace(' ', ''): Sustituye todos los espacios en blanco en la entrada del usuario por una cadena vacía, eliminándolos.
+# split(','): Divide la cadena resultante en una lista de elementos utilizando la coma como delimitador.
+#e.isalpha(): método de cadena Python que devuelve True si todos los caracteres son letras. Si está vacío o contiene letras devuelve False.
